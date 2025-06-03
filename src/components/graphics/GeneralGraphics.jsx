8d8ad2ff7eac050913e0cc78cc590c6dd98a3828
FraMor <lopezmoralesfrancisco18@gmail.com>
2025-06-03 22:29:29 +0200

    style: fixing title of graphic

--- src/components/graphics/GeneralGraphics.jsx
+++ src/components/graphics/GeneralGraphics.jsx
@@ -58,30 +58,32 @@ export default function IndexGraphic() {
   }, [session]);
 
   return (
-    <div className="w-full h-full -ml-10">
+    <div>
       <h2 className="text-lg font-semibold mb-2">Datos generales</h2>
-      <ResponsiveContainer width="100%" height="100%">
-        <PieChart>
-          <Pie
-            data={data}
-            dataKey="value"
-            nameKey="name"
-            cx="50%"
-            cy="50%"
-            innerRadius={70}
-            outerRadius={100}
-            fill="#8884d8"
-            paddingAngle={3}
-            label
-          >
-            {data.map((_, index) => (
-              <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-            ))}
-          </Pie>
-          <Tooltip />
-          <Legend layout="vertical" verticalAlign="middle" align="right" />
-        </PieChart>
-      </ResponsiveContainer>
+      <div className="w-full h-full -ml-10">
+        <ResponsiveContainer width="100%" height="100%">
+          <PieChart>
+            <Pie
+              data={data}
+              dataKey="value"
+              nameKey="name"
+              cx="50%"
+              cy="50%"
+              innerRadius={70}
+              outerRadius={100}
+              fill="#8884d8"
+              paddingAngle={3}
+              label
+            >
+              {data.map((_, index) => (
+                <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+              ))}
+            </Pie>
+            <Tooltip />
+            <Legend layout="vertical" verticalAlign="middle" align="right" />
+          </PieChart>
+        </ResponsiveContainer>
+      </div>
     </div>
   );
 }
