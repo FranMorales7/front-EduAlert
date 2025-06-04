969c7bfa8ec8cecd811e5240dc7c5b14b433958e
FraMor <lopezmoralesfrancisco18@gmail.com>
2025-06-04 11:01:00 +0200

    style: putting styles to the notification

--- src/components/notification/GlobalNotificationListener.jsx
+++ src/components/notification/GlobalNotificationListener.jsx
@@ -1,9 +1,34 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
 
 export default function GlobalNotificationListener() {
+
+  // Manejar notificación
+  function NotificationToast({ t, event }) {
+    const [expanded, setExpanded] = useState(false);
+  
+    return (
+      <div
+        onClick={() => setExpanded((prev) => !prev)}
+        className={`bg-white text-gray-900 border border-gray-300 px-4 py-3 rounded-lg shadow-md w-80 cursor-pointer transition-all duration-300 ${
+          t.visible ? "animate-enter" : "animate-leave"
+        }`}
+      >
+        <strong className="flex gap-2">
+          <BellRing className="bg-yellow-500" /> 
+          {event.title}
+        </strong>
+        {expanded && (
+          <p className="mt-2 text-sm text-gray-700 transition-opacity duration-300">
+            {event.message}
+          </p>
+        )}
+      </div>
+    );
+  }
+
   useEffect(() => {
     let echo;
     let channel;
@@ -16,10 +41,7 @@ export default function GlobalNotificationListener() {
 
       channel = echo.channel("notifications");
       channel.listen(".new-notification", (event) => {
-        toast.success(`🔔 ${event.title}`, {
-          description: event.message,
-          duration: 8000,
-        });
+        toast.custom((t) => <NotificationToast t={t} event={event} />);
       });
     };
 
